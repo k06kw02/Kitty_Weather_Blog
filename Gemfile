7840e2e50d9c3907809b018a8e60aaba607abeed
Katherine Wise <k06kw02@gmail.com>
2021-10-13 16:47:19 +0100

    set up storage yml correctly with encryption@

--- Gemfile
+++ Gemfile
@@ -23,13 +23,19 @@ gem 'redis', '~> 4.0'
 # gem 'bcrypt', '~> 3.1.7'
 
 # Use Active Storage variant
-# gem 'image_processing', '~> 1.2'
+gem 'image_processing', '~> 1.2'
 
 # Reduces boot times through caching; required in config/boot.rb
 gem 'bootsnap', '>= 1.4.4', require: false
 
+# Authorisation and Authentication
 gem 'devise'
 gem 'pundit'
+
+# dropbox active storage
+gem 'activestorage-dropbox'    # adapter for dropbox
+gem 'dropbox_api'              # required for `activestorage-dropbox`
+
 
 gem 'autoprefixer-rails', '10.2.5'
 gem 'font-awesome-sass'
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,33 @@
 class Post < ApplicationRecord
   belongs_to :user
+  has_many_attached :images
   validates :title, presence: true, length: { minimum: 2 }
   validates :content, presence: true, length: { minimum: 50 }
   validates :date, presence: true
   # validates :views, numericality: { only_integer: true }, presence: true
+  validate :acceptable_image
+  attr_accessor :remove_attached_image
+  after_save :purge_attached_image, if: :remove_attached_image?
+
+  # def acceptable image size and format
+  def acceptable_image
+    return unless image.attached?
+
+    if image.byte_size > 1.megabyte
+      image_size = (image.byte_size / 1_000_000.0).round(2)
+      errors.add(:image, "size #{image_size} MB exceeds 1 MB limit")
+    end
+
+    acceptable_types = ['image/jpeg', 'image/jpg', 'image/png', 'image/svg']
+    errors.add(:image, 'must be a JPEG, SVG or PNG') unless acceptable_types.include?(image.content_type)
+  end
+
+  # define the after_save action to remove an image
+  def remove_attached_image?
+    remove_attached_image == '1'
+  end
+
+  def purge_attached_image
+    image.purge_later
+  end
 end
