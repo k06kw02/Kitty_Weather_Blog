44999016a017de2f6c2cc387990de352ea239414
Katherine Wise <k06kw02@gmail.com>
2021-11-03 11:44:06 +0000

    a picture before I remove my active model and wsysiwyg

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,35 +1,36 @@
 class Post < ApplicationRecord
   belongs_to :user
-  has_many_attached :images
-  has_rich_text :rich_content
+  # has_one_attached :photo
+  # has_many_attached :images
+  # has_rich_text :rich_content
 
   validates :title, presence: true, length: { minimum: 2 }
-  validates :rich_content, presence: true, length: { minimum: 50 }
+  # validates :rich_content, presence: true, length: { minimum: 50 }
   validates :date, presence: true
   # validates :views, numericality: { only_integer: true }, presence: true
-  # validate :acceptable_image
-  # attr_accessor :remove_attached_image
-  # after_save :purge_attached_image, if: :remove_attached_image?
+  # validate :acceptable_photo
+  # attr_accessor :remove_attached_photo
+  # after_save :purge_attached_photo, if: :remove_attached_photo?
 
-  # # def acceptable image size and format
-  # def acceptable_image
-  #   return unless image.attached?
+  # def acceptable photo size and format
+#   def acceptable_photo
+#     return unless photo.attached?
 
-  #   if image.byte_size > 1.megabyte
-  #     image_size = (image.byte_size / 1_000_000.0).round(2)
-  #     errors.add(:image, "size #{image_size} MB exceeds 1 MB limit")
-  #   end
+#     if photo.byte_size > 1.megabyte
+#       photo_size = (photo.byte_size / 1_000_000.0).round(2)
+#       errors.add(:photo, "size #{photo_size} MB exceeds 1 MB limit")
+#     end
 
-  #   acceptable_types = ['image/jpeg', 'image/jpg', 'image/png', 'image/svg']
-  #   errors.add(:image, 'must be a JPEG, SVG or PNG') unless acceptable_types.include?(image.content_type)
-  # end
-
-  # define the after_save action to remove an image
-#   def remove_attached_image?
-#     remove_attached_image == '1'
+#     acceptable_types = ['photo.jpeg', 'photo.jpg', 'photo/png', 'photo/svg']
+#     errors.add(:photo, 'must be a JPEG, SVG or PNG') unless acceptable_types.include?(photo.content_type)
 #   end
 
-#   def purge_attached_image
-#     image.purge_later
+#   # define the after_save action to remove an photo
+#   def remove_attached_photo?
+#     remove_attached_photo == '1'
+#   end
+
+#   def purge_attached_photo
+#     photo.purge_later
 #   end
 end
